4366799b2676f87cb28cf918e3e9d4646cae4f24
Ann Addicks <ann.addicks@gmail.com>
2017-06-13 16:11:35 -0400

    set the board members based on sheet 2

--- sheets.go
+++ sheets.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+
+   "fmt"
 	"io/ioutil"
 
 	"golang.org/x/net/context"
@@ -13,9 +15,10 @@ type Board struct {
 }
 type AgendaRoles struct {
 	toastmaster, ge, timer, ahCounter, grammarian, eval1, speaker1, eval2, speaker2, eval3, speaker3, eval4, speaker4, tableTopicsMaster string
+	boardMembers Board
 }
 
-func getSheet() *spreadsheet.Sheet {
+func getSheet() (*spreadsheet.Sheet, *spreadsheet.Sheet) {
 	data, err := ioutil.ReadFile("client_secret.json")
 	if err != nil {
 		panic("cannot read client_secret.json")
@@ -34,47 +37,37 @@ func getSheet() *spreadsheet.Sheet {
 		panic("cannot fetch spread sheet: ")
 	}
 
-	sheet, err := spreadsheet.SheetByIndex(0)
+	roles, err := spreadsheet.SheetByIndex(0)
 	if err != nil {
-		panic("Cannot read spreadsheet by index")
+		panic("Cannot read spreadsheet by index 0")
 	}
 
-	return sheet
+	board, err := spreadsheet.SheetByIndex(1)
+	if err != nil {
+		panic("Cannot read spreadsheet by index 1")
+	}
+
+	return roles, board
 }
 
-/*func getBoard() Board {
-	data, err := ioutil.ReadFile("client_secret.json")
-	if err != nil {
-		panic("cannot read client_secret.json")
-	}
+func getBoard(sheet *spreadsheet.Sheet) Board {
+	board := Board{}
+	board.president = sheet.Columns[1][0].Value
+	board.vpe = sheet.Columns[1][1].Value
+	board.vppr = sheet.Columns[1][2].Value
+	board.secretary = sheet.Columns[1][3].Value
+	board.treasurer = sheet.Columns[1][4].Value
+	board.saa = sheet.Columns[1][5].Value	
 
-	conf, err := google.JWTConfigFromJSON(data, spreadsheet.Scope)
-	if err != nil {
-		panic("problem with google.JWTConfigFromJSON(data, spreadsheet.Scope)")
-	}
-
-	client := conf.Client(context.TODO())
-
-	service := spreadsheet.NewServiceWithClient(client)
-	spreadsheet, err := service.FetchSpreadsheet("1_P9K2asfsITSGEAh7PrPLxncSemNnjXHg3_O3q7OW0k/")
-	if err != nil {
-		panic("cannot fetch spread sheet")
-	}
-
-	sheet2, err := spreadsheet.SheetByIndex(1)
-	if err != nil {
-		panic("Cannot read spreadsheet by index")
-	}
-
-
-	return Board{}//convertSheetToBoard(sheet2)
-}*/
+	return board
+}
 
 func GetRoles(agendaDate string) AgendaRoles {
-	sheet := getSheet()
-	//board := getBoard()
+	sheet, roles := getSheet()
+	boardMembers := getBoard(roles)
 
 	agendaRoles := AgendaRoles{}
+	agendaRoles.boardMembers = boardMembers
 
 	for i := range sheet.Columns {
 		if sheet.Columns[i][0].Value == agendaDate {
