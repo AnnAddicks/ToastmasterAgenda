3a51d1920925b5a959a9fd8f1e011c7739af0b5a
Ann Addicks <ann.addicks@gmail.com>
2017-10-30 12:58:58 -0400

    removed redudnant type data.

--- speech.go
+++ speech.go
@@ -58,69 +58,69 @@ type manual struct {
 
 // Representation of the Toasmaters speeches with their code as the key.
 var manualMap = map[string]manual{
-	"cc": manual{
+	"cc": {
 		manualName: "Competent Communicator",
 		speeches: []speechDetails{
-			speechDetails{number: 1, name: "Ice Breaker", min: 4, max: 6},
-			speechDetails{number: 2, name: "Organize Your Speech", min: 5, max: 7},
-			speechDetails{number: 3, name: "Get to the Point", min: 5, max: 7},
-			speechDetails{number: 4, name: "How to Say It", min: 5, max: 7},
-			speechDetails{number: 5, name: "Your Body Speaks", min: 5, max: 7},
-			speechDetails{number: 6, name: "Vocal Variety", min: 5, max: 7},
-			speechDetails{number: 7, name: "Research Your Topic", min: 5, max: 7},
-			speechDetails{number: 8, name: "Visual Aids", min: 5, max: 7},
-			speechDetails{number: 9, name: "Persuade with Power", min: 5, max: 7},
-			speechDetails{number: 10, name: "Inspire Your Audience", min: 8, max: 10},
+			{number: 1, name: "Ice Breaker", min: 4, max: 6},
+			{number: 2, name: "Organize Your Speech", min: 5, max: 7},
+			{number: 3, name: "Get to the Point", min: 5, max: 7},
+			{number: 4, name: "How to Say It", min: 5, max: 7},
+			{number: 5, name: "Your Body Speaks", min: 5, max: 7},
+			{number: 6, name: "Vocal Variety", min: 5, max: 7},
+			{number: 7, name: "Research Your Topic", min: 5, max: 7},
+			{number: 8, name: "Visual Aids", min: 5, max: 7},
+			{number: 9, name: "Persuade with Power", min: 5, max: 7},
+			{number: 10, name: "Inspire Your Audience", min: 8, max: 10},
 		},
 	},
-	"inform": manual{
+	"inform": {
 		manualName: "Speaking to Inform",
 		speeches: []speechDetails{
-			speechDetails{number: 1, name: "The Speech to Inform", min: 5, max: 7},
-			speechDetails{number: 2, name: "Resources for Informing", min: 5, max: 7},
-			speechDetails{number: 3, name: "The Demonstration Talk", min: 5, max: 7},
-			speechDetails{number: 4, name: "A Fact Finding Report", min: 5, max: 7},
-			speechDetails{number: 5, name: "The Abstract Concept", min: 6, max: 8},
+			{number: 1, name: "The Speech to Inform", min: 5, max: 7},
+			{number: 2, name: "Resources for Informing", min: 5, max: 7},
+			{number: 3, name: "The Demonstration Talk", min: 5, max: 7},
+			{number: 4, name: "A Fact Finding Report", min: 5, max: 7},
+			{number: 5, name: "The Abstract Concept", min: 6, max: 8},
 		},
 	},
-	"interpretive": manual{
+	"interpretive": {
 		manualName: "Interpretive Reading",
 		speeches: []speechDetails{
-			speechDetails{number: 1, name: "Read a Story", min: 8, max: 10},
-			speechDetails{number: 2, name: "Interpretive Poetry", min: 6, max: 8},
-			speechDetails{number: 3, name: "The Monodrama", min: 5, max: 7},
-			speechDetails{number: 4, name: "The Play", min: 12, max: 15},
-			speechDetails{number: 5, name: "The Oratorical Speech", min: 8, max: 10},
+			{number: 1, name: "Read a Story", min: 8, max: 10},
+			{number: 2, name: "Interpretive Poetry", min: 6, max: 8},
+			{number: 3, name: "The Monodrama", min: 5, max: 7},
+			{number: 4, name: "The Play", min: 12, max: 15},
+			{number: 5, name: "The Oratorical Speech", min: 8, max: 10},
 		},
 	},
-	"mgt": manual{
+	"mgt": {
 		manualName: "Speeches By Management",
 		speeches: []speechDetails{
-			speechDetails{number: 1, name: "The Briefing", min: 8, max: 10},
-			speechDetails{number: 2, name: "The Technical Speech", min: 8, max: 10},
-			speechDetails{number: 3, name: "Manage and Motivate", min: 10, max: 12},
-			speechDetails{number: 4, name: "The Status Report", min: 10, max: 12},
-			speechDetails{number: 5, name: "Confrontation", min: 5, max: 15},
+			{number: 1, name: "The Briefing", min: 8, max: 10},
+			{number: 2, name: "The Technical Speech", min: 8, max: 10},
+			{number: 3, name: "Manage and Motivate", min: 10, max: 12},
+			{number: 4, name: "The Status Report", min: 10, max: 12},
+			{number: 5, name: "Confrontation", min: 5, max: 15},
 		},
 	},
-	"technical": manual{
+	"technical": {
 		manualName: "Technical Presentations",
 		speeches: []speechDetails{
-			speechDetails{number: 1, name: "Technical Briefing", min: 8, max: 10},
-			speechDetails{number: 2, name: "The Proposal", min: 8, max: 10},
-			speechDetails{number: 3, name: "The Nontechnical Audience", min: 10, max: 12},
-			speechDetails{number: 4, name: "Presenting a Technical Paper", min: 10, max: 12},
-			speechDetails{number: 5, name: "The Team Technical Presentation", min: 20, max: 30},
+			{number: 1, name: "Technical Briefing", min: 8, max: 10},
+			{number: 2, name: "The Proposal", min: 8, max: 10},
+			{number: 3, name: "The Nontechnical Audience", min: 10, max: 12},
+			{number: 4, name: "Presenting a Technical Paper", min: 10, max: 12},
+			{number: 5, name: "The Team Technical Presentation", min: 20, max: 30},
 		},
 	},
-	"storytelling": manual{
+	"storytelling": {
 		manualName: "Storytelling",
 		speeches: []speechDetails{
-			speechDetails{number: 1, name: "The Folk Tale", min: 7, max: 9},
-			speechDetails{number: 2, name: "Let's Get Personal", min: 6, max: 8},
-			speechDetails{number: 3, name: "The Moral of the Story", min: 4, max: 6},
-			speechDetails{number: 4, name: "The Touching Story", min: 6, max: 8},
-			speechDetails{number: 5, name: "Bringing History to Life", min: 7, max: 9},
+			{number: 1, name: "The Folk Tale", min: 7, max: 9},
+			{number: 2, name: "Let's Get Personal", min: 6, max: 8},
+			{number: 3, name: "The Moral of the Story", min: 4, max: 6},
+			{number: 4, name: "The Touching Story", min: 6, max: 8},
+			{number: 5, name: "Bringing History to Life", min: 7, max: 9},
 		},
 	},
 }
